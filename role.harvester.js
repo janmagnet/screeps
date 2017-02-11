f2b0a9f6f79c5fe5ea63e873dad2ac2f25a8ce6b
Jan Magne Tjensvold <janmagnet@users.noreply.github.com>
2017-02-11 17:29:25 +0100

    Harvesters prioritize first unloading into spawn and extensions, then into towers, and finally into storage and containers

--- role.harvester.js
+++ role.harvester.js
@@ -13,32 +13,63 @@ module.exports = {
         }
 
         if (isWorking == true) {
-            var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                filter: (s) => ((s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity) || (s.structureType == STRUCTURE_STORAGE && s.storeCapacity - _.sum(s.store) > 0)
-            });
-            
-            if (structure != undefined) {
-                if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structure);
-                }
-            } else {
-                structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                    filter: (s) => s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity
-                });
+            var canTransferEnergyToSpawnAndExtensions = module.exports.transferEnergyToSpawnAndExtensions(creep);
+            if (canTransferEnergyToSpawnAndExtensions) return;
 
-                if (structure != undefined) {
-                    if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(structure);
-                    }
-                } else {
-                    roleUpgrader.run(creep);
-                }
-            }
+            var canTransferEnergyToTower = module.exports.transferEnergyToTower(creep);
+            if (canTransferEnergyToTower) return;
+
+            var canTransferEnergyToStorageAndContainer = module.exports.transferEnergyToStorageAndContainer(creep);
+            if (canTransferEnergyToStorageAndContainer) return;
+
+            roleUpgrader.run(creep);
         } else {
             var foundEnergy = harvestEnergy.harvestEnergy(creep);
             if (!foundEnergy && creep.carry.energy > 0) {
                 memory.working = true;
             }
         }
+    },
+
+    transferEnergyToSpawnAndExtensions: function(creep) {
+        var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+            filter: (s) => (s.structureType == STRUCTURE_SPAWN || s.structureType == STRUCTURE_EXTENSION) && s.energy < s.energyCapacity
+        });
+        
+        if (structure == undefined) return false;
+
+        if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(structure);
+        }
+        
+        return true;
+    },
+
+    transferEnergyToTower: function(creep) {
+        var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+            filter: (s) => s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity
+        });
+        
+        if (structure == undefined) return false;
+
+        if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(structure);
+        }
+        
+        return true;
+    },
+
+    transferEnergyToStorageAndContainer: function(creep) {
+        var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+            filter: (s) => (s.structureType == STRUCTURE_STORAGE || s.structureType == STRUCTURE_CONTAINER) && s.storeCapacity - _.sum(s.store) > 0
+        });
+        
+        if (structure == undefined) return false;
+
+        if (creep.transfer(structure, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(structure);
+        }
+        
+        return true;
     }
 };
